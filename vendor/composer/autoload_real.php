885d54ff2eaba1b20ab10e15a10a685f499bbcb7
mlemke24 <matt@missionlab.dev>
2021-09-27 13:21:12 -0500

    initial scss setup

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInit4b587b6279af2fccb017fd2bd54b682b
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit4b587b6279af2fccb017fd2bd54b682b', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit4b587b6279af2fccb017fd2bd54b682b', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,42 +1,32 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f72ed1004c046183a63d7cc5c2900a4eb963c7c7',
+        'name' => 'church-plugins/cp-library',
+        'dev' => true,
     ),
-    'reference' => 'c6eaa56a897f7a2e877564a6e4544db6cd0559eb',
-    'name' => 'church-plugins/cp-library',
-  ),
-  'versions' => 
-  array (
-    'church-plugins/cp-library' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'c6eaa56a897f7a2e877564a6e4544db6cd0559eb',
+    'versions' => array(
+        'church-plugins/cp-library' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f72ed1004c046183a63d7cc5c2900a4eb963c7c7',
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v2.0.1',
+            'version' => '2.0.1.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'a241e78aaeb09781f5f5b92ac01ffd13ab43e5e8',
+            'dev_requirement' => false,
+        ),
     ),
-    'cmb2/cmb2' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'cacbc8cedbfdf8ffe0e840858e6860f9333c33f2',
-    ),
-    'composer/installers' => 
-    array (
-      'pretty_version' => 'v2.0.1',
-      'version' => '2.0.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'a241e78aaeb09781f5f5b92ac01ffd13ab43e5e8',
-    ),
-  ),
 );
