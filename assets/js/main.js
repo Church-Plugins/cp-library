5d861bae618d82c3ba28eed5016f7ebeac0884d5
vicky-carbon <vicky@carbonhealth.com>
2021-10-14 01:10:37 -0400

    Utilize the persistent layer for individual talk
    If the persistent layer is active, use that instead of the individual
    player. If not, then use the individual player.
    
    When going back to the list of talks from an individual talk, if the
    audio is still playing, hand it off to the persistent player. There's a
    slight kink in that transition as we only update the progress every so
    often. Lowering the progress update interval can give us smoother
    transition but may have performance impact. Adjust cautiously.
    
    <PersistentPlayerProvider /> is where all the syncing and communicating
    happen between the persistent player app and the consumer. It always
    use the top-most window to support the iframe navigation trick.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@
 		let SELF = this;
 
 		SELF.init = function() {
+			console.log("persistentPlayer.js initializing");
 			SELF.$body = $('body');
 			SELF.$iframe = false;
 			SELF.isIframe = (window !== window.parent);
@@ -12,24 +13,53 @@
 
 			SELF.$body.on('click', 'a', SELF.handleLinkClick);
 			window.addEventListener("message", SELF.iframeMessage);
+			console.log("persistentPlayer.js initialized", {
+				body: SELF.$body,
+				iframe: SELF.$iframe,
+				isIframe: SELF.isIframe,
+			});
 		};
 
+		/**
+		 * Indicates whether the persistent player component has been mounted or not.
+		 * @returns boolean
+		 */
 		SELF.isActive = function() {
+			console.log("persistentPlayer.js isActive:", SELF.$body.hasClass('cpl-persistent-player'))
 			return SELF.$body.hasClass('cpl-persistent-player');
 		};
 
+		/**
+		 * Invoked whenever a link (<a />) is clicked anywhere in the body. If it's a first-party link,
+		 * we hijack the event.
+		 * @param {MouseEvent} e 
+		 * @returns false
+		 */
 		SELF.handleLinkClick = function(e) {
+			console.log("persistentPlayer.js handle link click", { e })
 			SELF.url = e.currentTarget.href;
 
 			// make sure this is a local link
 			if (!SELF.url.includes(window.location.hostname)) {
+				console.log("persistentPlayer.js not a local link", {
+					url: SELF.url,
+					hostname: window.location.hostname,
+				})
 				return;
 			}
 
 			return SELF.isIframe ? SELF.handleIframeClick() : SELF.handleClick();
 		};
 
+		/**
+		 * Invoked when we're in an iframe. Sends a message to the top-most iframe.
+		 * @returns false
+		 */
 		SELF.handleIframeClick = function() {
+			console.log("persistentPlayer.js handling iframe click", {
+				'action': SELF.messageAction,
+				'url'   : SELF.url,
+			});
 			window.top.postMessage({
 				'action': SELF.messageAction,
 				'url'   : SELF.url,
@@ -38,8 +68,15 @@
 			return false;
 		};
 
+		/**
+		 * Invoked when we're the top-level window. Attach a new iframe to the body whose `src` is the
+		 * destination URL.
+		 * @returns boolean
+		 */
 		SELF.handleClick = function() {
+			console.log("persistentPlayer.js handling click");
 			if ( !SELF.isActive() ) {
+				console.log("persistentPlayer.js is not active");
 				return true;
 			}
 
@@ -48,11 +85,19 @@
 
 			SELF.$iframe.on('load', SELF.iframeLoaded);
 			SELF.$iframe.attr('src', SELF.url);
+			console.log("persistentPlayer.js iframe attached")
 
 			return false;
 		};
 
+		/**
+		 * Invoked when the newly-attached iframe loaded. Remove everything outside the iframe but the
+		 * persistent player. This makes it look like the page has successfully navigated while the
+		 * player persist. A SPA-like experience as far the end-user is concerned.
+		 */
 		SELF.iframeLoaded = function() {
+			console.log("persistentPlayer.js iframe loaded");
+
 			window.history.pushState({}, '', url);
 
 			$('body > *').each(function () {
@@ -64,16 +109,27 @@
 
 				$(this).remove();
 			});
+			console.log("persistentPlayer.js stuf removed");
 		};
 
+		/**
+		 * Invoked whenever someone post a message to this window.
+		 * @param {MessageEvent} e 
+		 * @returns undefined
+		 */
 		SELF.iframeMessage = function(e) {
+			// console.log("persistentPlayer.js message received", {e})
+			// Filter out anything that we don't care about
 			if (SELF.messageAction !== e.data.action) {
+				console.log("persistentPlayer.js message ignored", e.data);
 				return;
 			}
 
 			if (SELF.isActive()) {
+				console.log("persistentPlayer.js changing iframe src", e.data.url)
 				SELF.$iframe.attr('src', e.data.url);
 			} else {
+				console.log("persistentPlayer.js changing URL", e.data.url);
 				window.location.href = e.data.url;
 			}
 		};
@@ -82,6 +138,7 @@
 	};
 
 	$(document).on('ready', function() {
+		console.log("invoking persistentPlayer.js")
 		persistentPlayer();
 	});
 
