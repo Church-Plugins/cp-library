a286e7096ab4829a64149fa1a580121e1c0879a1
Tanner Moushey <tanner@missionlab.dev>
2022-02-17 11:26:04 -0600

    Fix talk player

--- app/src/Components/ItemDetail.jsx
+++ app/src/Components/ItemDetail.jsx
@@ -387,7 +387,7 @@ export default function ItemDetail({
 						          )}
 					          </>
 				          ) : (
-					          <Logo style="height:50%;"/>
+					          <Logo/>
 				          )}
 			          </Box>
 		          )}
