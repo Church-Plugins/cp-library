43b8c25ae44fc4c1dd858c04ad8e95d0167ad72f
Tanner Moushey <tanner@missionlab.dev>
2022-03-13 19:34:20 -0500

    move CMB2 to ChurchPlugins repo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b2e388deef439a032994bfe0b88bb9fc0d346d0d',
+    'name' => 'churchplugins/cp-library',
+  ),
+  'versions' => 
+  array (
+    'churchplugins/cp-library' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b2e388deef439a032994bfe0b88bb9fc0d346d0d',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a241e78aaeb09781f5f5b92ac01ffd13ab43e5e8',
+    ),
+    'wpackio/enqueue' => 
+    array (
+      'pretty_version' => '3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8a5604f5ab3bd5f000e503dda02603e15e534d47',
+    ),
+  ),
+);
