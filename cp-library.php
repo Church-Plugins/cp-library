b784c26be804d706f221b9e4a2985ff44aea1140
Tanner Moushey <tanner@missionlab.dev>
2022-07-20 13:26:05 -0500

    Update to use new include method for core

--- cp-library.php
+++ cp-library.php
@@ -12,10 +12,7 @@
 
 require_once( dirname( __FILE__ ) . "/includes/Constants.php" );
 
-if ( ! class_exists( 'ChurchPlugins' ) ) {
-	require_once( CP_LIBRARY_PLUGIN_DIR . "/includes/ChurchPlugins/init.php" );
-}
-
+require_once( CP_LIBRARY_PLUGIN_DIR . "/includes/ChurchPlugins/init.php" );
 require_once( CP_LIBRARY_PLUGIN_DIR . 'vendor/autoload.php' );
 
 use CP_Library\Init as CP_Init;
