978917e778311c77c611d51494a35651911e034e
Jonathan Roley <jonathan.roley@gmail.com>
2023-08-02 10:06:00 -0400

    show warning for incompatible post type

--- blocks/sermon-speaker/edit.js
+++ blocks/sermon-speaker/edit.js
@@ -17,16 +17,25 @@ import { __ } from '@wordpress/i18n';
 
 
 export default function SermonSpeakerEdit({
-	context: { item }
+	context: { postType, item }
 }) {
-	const speakers = item?.speakers || []
 
 	const blockProps = useBlockProps({})
+
+	if( postType !== 'cpl_item' ) {
+		return (
+			<div {...blockProps}>{ __( 'This block is not compatible with this post type', 'cp-library' ) }</div>
+		)
+	}
+
+	const speakers = item?.speakers || []
+
 
 	return (
 		<>
 			<div {...blockProps}>
 			<span className='material-icons-outlined'>person</span>
+			
 			{
 				speakers.length ?
 				speakers.map((speaker, index) => (
