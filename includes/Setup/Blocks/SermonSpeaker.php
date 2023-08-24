3ec9d937492e5d7e5b3e71781e39927cd65eb9c7
Jonathan Roley <jonathan.roley@gmail.com>
2023-08-24 15:40:33 -0400

    initial speaker permalinks

--- includes/Setup/Blocks/SermonSpeaker.php
+++ includes/Setup/Blocks/SermonSpeaker.php
@@ -1,56 +1,65 @@
 <?php
 
 namespace CP_Library\Setup\Blocks;
+
+use CP_Library\Admin\Settings;
 use CP_Library\Setup\Blocks\Block;
 use CP_Library\Models\Speaker as Speaker_Model;
 use CP_Library\Models\Item as Item_Model;
 
 class SermonSpeaker extends Block {
-    public $name = 'sermon-speaker';
-    public $is_dynamic = true;
+	public $name = 'sermon-speaker';
+	public $is_dynamic = true;
 
-    public function __construct() {
-      parent::__construct();
-    }
+	public function __construct() {
+		parent::__construct();
+	}
 
-    /**
-     * Renders the `cp-library/sermon-speaker` block on the server.
-     *
-     * @param array    $attributes Block attributes.
-     * @param string   $content    Block default content.
-     * @param \WP_Block $block      Block instance.
-     * @return string Returns the HTML for the sermon speaker.
-     */
-    function render( $attributes, $content, $block ) {
-      if( ! isset( $block->context['postId'] ) || $block->context['postType'] !== 'cpl_item' ) {
-        return '';
-      }
+	/**
+	 * Renders the `cp-library/sermon-speaker` block on the server.
+	 *
+	 * @param array    $attributes Block attributes.
+	 * @param string   $content    Block default content.
+	 * @param \WP_Block $block      Block instance.
+	 * @return string Returns the HTML for the sermon speaker.
+	 */
+	function render( $attributes, $content, $block ) {
+		if( ! isset( $block->context['postId'] ) || $block->context['postType'] !== 'cpl_item' ) {
+			return '';
+		}
 
-      $item = new \CP_Library\Controllers\Item( $block->context['postId'], true );
+		$item = new \CP_Library\Controllers\Item( $block->context['postId'], true );
 
-      $speakers = $item->get_speakers();
+		$speakers = $item->get_speakers();
 
-      if( count( $speakers ) === 0 ) {
-        return '';
-      }
-      
-      $wrapper_attributes = get_block_wrapper_attributes();
+		if( count( $speakers ) === 0 ) {
+			return '';
+		}
+		
+		$wrapper_attributes = get_block_wrapper_attributes();
 
-      $output = sprintf( '<div %1$s>', $wrapper_attributes );
-      $output .= '<span class="material-icons-outlined">person</span>';
+		$output = sprintf( '<div %1$s>', $wrapper_attributes );
+		$output .= '<span class="material-icons-outlined">person</span>';
 
-      $speakers_arr = array();
+		$speakers_arr = array();
 
-      foreach( $speakers as $speaker ) {
-        $speakers_arr[] = sprintf( '<span class="cpl-speaker-link">%1$s', esc_html( $speaker['title'] ) );
-      }
+		foreach( $speakers as $speaker ) {
+			$title = $speaker['title'];
 
-      $output .= implode( ',</a>', $speakers_arr );
+			if( Settings::get_speaker( 'enable_permalinks', false ) ) {
+				$url = get_permalink( $speaker['origin_id'] );
+				$title = sprintf( '<a href="%1$s" class="cpl-speaker-link">%2$s</a>', $url, $title );
+			}
 
-      $output .= '</a>';
+			$speakers_arr[] = sprintf( '<span class="cpl-speaker-link">%1$s', $title );
+		}
 
-      $output .= '</div>';
+		$output .= implode( ',</span>', $speakers_arr );
 
-      return $output;
-    }
+		$output .= '</span>';
+
+		$output .= '</div>';
+
+		return $output;
+	}
 }
