5c38b1626f12d73360ed429714d156f483800e56
Jonathan Roley <jonathan.roley@gmail.com>
2023-09-18 11:48:20 -0400

    code improvements

--- includes/Admin/Migrate/SetupWizard.php
+++ includes/Admin/Migrate/SetupWizard.php
@@ -123,7 +123,7 @@ class SetupWizard {
 			<div class="postbox">
 				<div class="inside">
 					<h1>CP Library Migration Wizard</h1>
-					<p>Thank you for choosing CP Library! It looks like you have content created with another sermon managing plugin. Would you like to run an automatic migration?</p>
+					<p>It looks like you have content created with another sermon managing plugin. Would you like to run an automatic migration?</p>
 					<div id="cpl-migration-root" data-details="<?php echo esc_attr( wp_json_encode( $migrations ) ); ?>"></div>
 				</div>
 			</div>
