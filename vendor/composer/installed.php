99b54c916fcbf8359d59fa1b6e9b3367e7e1f309
Tanner Moushey <tanner@missionlab.dev>
2023-09-29 20:57:00 -0500

    re-add vendor directory

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4afbc5f174287311faf2ddeb79c671d649dc581a',
+    'name' => 'churchplugins/cp-library',
+  ),
+  'versions' => 
+  array (
+    'churchplugins/cp-library' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4afbc5f174287311faf2ddeb79c671d649dc581a',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a241e78aaeb09781f5f5b92ac01ffd13ab43e5e8',
+    ),
+    'wpackio/enqueue' => 
+    array (
+      'pretty_version' => '3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8a5604f5ab3bd5f000e503dda02603e15e534d47',
+    ),
+  ),
+);
