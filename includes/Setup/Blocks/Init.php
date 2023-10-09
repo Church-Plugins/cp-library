a4ab0ceb393b33de56f76210624665ec416ca499
Jonathan Roley <jonathan.roley@gmail.com>
2023-10-09 09:19:08 -0400

    change default image size to full so that attachment id can be retrieved via URL

--- includes/Setup/Blocks/Init.php
+++ includes/Setup/Blocks/Init.php
@@ -72,35 +72,7 @@ class Init {
 	 */
 	protected function actions() {
 		add_action( 'init', array( $this, 'register_block_patterns' ) );
-		add_filter( 'default_post_metadata', array( $this, 'default_thumbnail' ), 10, 5 );
 		add_filter( 'block_categories_all', array( $this, 'block_categories' ) );
-	}
-
-	/**
-	 * Sets default thumbnail for sermon and series post types
-	 *
-	 * @param mixed  $value the value of the meta field.
-	 * @param int    $object_id the post ID.
-	 * @param string $meta_key the meta key.
-	 * @param bool   $single whether to return a single value.
-	 * @param string $meta_type the type of metadata.
-	 * @author Jonathan Roley
-	 */
-	public function default_thumbnail( $value, $object_id, $meta_key, $single, $meta_type ) {
-		if ( $value ) {
-			return $value;
-		}
-		if ( '_thumbnail_id' === $meta_key && 'post' === $meta_type ) {
-			$post_type = get_post_type( $object_id );
-			if ( cp_library()->setup->post_types->item->post_type === $post_type || cp_library()->setup->post_types->item_type->post_type === $post_type ) {
-				$image = Settings::get( 'default_thumbnail', false );
-				if ( $image ) {
-					$image = attachment_url_to_postid( $image );
-				}
-				return $image ? $image : $value;
-			}
-		}
-		return $value;
 	}
 
 	/**
