a3fe8bae0c882e9c6174a17bd1c6ae0060e696d7
Tanner Moushey <tanner@missionlab.dev>
2023-10-19 12:37:00 -0500

    Update block patterns

--- block-patterns/latest-series.php
+++ block-patterns/latest-series.php
@@ -21,14 +21,14 @@ return array(
 <!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:cp-library/item-title {"level":3,"isLink":true} /-->
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"var:preset|spacing|40","margin":{"top":"var:preset|spacing|20"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--20);padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:cp-library/item-date /--></div>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"var:preset|spacing|40","margin":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:cp-library/item-date {"fontSize":"small"} /--></div>
 <!-- /wp:group -->
 
 <!-- wp:cp-library/item-description {"moreText":"Read More","excerptLength":25,"style":{"spacing":{"margin":{"bottom":"0","left":"0","right":"0","top":"var:preset|spacing|40"}}}} /-->
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:cp-library/sermon-scripture {"style":{"border":{"color":"#e5e8ef","width":"1px"},"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}}} /--></div>
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:cp-library/sermon-scripture {"style":{"border":{"color":"#e5e8ef","width":"1px"},"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"fontSize":"small"} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
--- block-patterns/series-grid.php
+++ block-patterns/series-grid.php
@@ -10,20 +10,20 @@ return array(
 	'blockTypes' => array( 'cp-library/query' ),
 	'categories' => array( 'cpl_item_type' ),
 	'content'    => '<!-- wp:cp-library/query {"queryId":2,"query":{"perPage":"6","pages":0,"offset":0,"postType":"cpl_item_type","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"include":[],"sticky":"","inherit":false,"taxQuery":null,"cpl_speakers":null,"cpl_service_types":null,"parents":[]},"displayLayout":{"type":"flex","columns":2},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-cp-library-query"><!-- wp:cp-library/sermon-template {"style":{"spacing":{"padding":{"top":"0","bottom":"0"}}}} -->
-	<!-- wp:group {"style":{"spacing":{"padding":{"left":"24px","right":"24px","top":"24px","bottom":"24px"}},"color":{"background":"#f0f0f0"},"dimensions":{"minHeight":"100%"},"border":{"radius":"8px"}},"layout":{"inherit":true,"type":"constrained"}} -->
-	<div class="wp-block-group has-background" style="border-radius:8px;background-color:#f0f0f0;min-height:100%;padding-top:24px;padding-right:24px;padding-bottom:24px;padding-left:24px"><!-- wp:cp-library/item-graphic {"isLink":true,"aspectRatio":"16/9","style":{"color":{}}} -->
-	<div class="wp-block-cp-library-item-graphic"></div>
-	<!-- /wp:cp-library/item-graphic -->
+<div class="wp-block-cp-library-query"><!-- wp:cp-library/sermon-template {"style":{"spacing":{"padding":{"top":"0","bottom":"0"}}}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"left":"var:preset|spacing|50","right":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}},"color":{"background":"#f0f0f0"},"border":{"radius":"8px"},"dimensions":{"minHeight":"100%"}},"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group has-background" style="border-radius:8px;background-color:#f0f0f0;min-height:100%;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:cp-library/item-graphic {"isLink":true,"aspectRatio":"16/9"} -->
+<div class="wp-block-cp-library-item-graphic"></div>
+<!-- /wp:cp-library/item-graphic -->
 
-	<!-- wp:cp-library/item-title {"level":3,"isLink":true,"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"margin":{"top":"16px"}},"typography":{"fontSize":"1.4em"}}} /-->
+<!-- wp:cp-library/item-title {"level":3,"isLink":true,"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"margin":{"top":"var:preset|spacing|40"}}},"fontSize":"medium"} /-->
 
-	<!-- wp:cp-library/item-date {"textAlign":"left","style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"right":"0","left":"0","top":"8px","bottom":"8px"}}}} /-->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"var:preset|spacing|40","margin":{"top":"var:preset|spacing|40"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--40);padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:cp-library/item-date {"textAlign":"left","fontSize":"small"} /-->
 
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"16px","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-	<div class="wp-block-group" style="margin-top:16px;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:cp-library/sermon-scripture {"style":{"spacing":{"padding":{"top":"4px","bottom":"4px","left":"8px","right":"8px"},"margin":{"top":"0","bottom":"0","left":"0","right":"0"}},"border":{"color":"#e5e8ef"}}} /--></div>
-	<!-- /wp:group --></div>
-	<!-- /wp:group -->
-	<!-- /wp:cp-library/sermon-template --></div>
-	<!-- /wp:cp-library/query -->',
+<!-- wp:cp-library/sermon-scripture {"fontSize":"small"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+<!-- /wp:cp-library/sermon-template --></div>
+<!-- /wp:cp-library/query -->',
 );
