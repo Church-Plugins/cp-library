d4abb29e81da7543f5bff2a0bb2d02f78d152ac9
Jonathan Roley <jonathan.roley@gmail.com>
2024-01-19 11:53:28 -0500

    tweak attachment styling

--- templates/parts/item-single/attachments.php
+++ templates/parts/item-single/attachments.php
@@ -11,9 +11,9 @@ $downloads = get_post_meta( get_the_ID(), 'downloads', true );
 
 <?php if ( ! empty( $downloads ) ) : ?>
 	<div class="cpl-single-item--attachments">
-		<span class="material-icons-outlined">attach_file</span>
 		<?php foreach ( $downloads as $download ) : ?>
-			<a href="<?php echo esc_url( $download['file'] ); ?>" target="_blank" rel="noopener noreferrer">
+			<a class="cpl-single-item--attachment href="<?php echo esc_url( $download['file'] ); ?>" target="_blank" rel="noopener noreferrer">
+				<span class="material-icons-outlined">attach_file</span>
 				<?php echo esc_html( empty( $download['name'] ) ? $download['file'] : $download['name'] ); ?>
 			</a>
 		<?php endforeach; ?>
