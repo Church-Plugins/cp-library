f7a7abbb2f1ac386ff6115e0d39b5a2d100c12c2
Jonathan Roley <jonathan.roley@gmail.com>
2024-09-24 17:51:09 -0400

    sermon audio fetching improvements

--- includes/Adapters/Adapter.php
+++ includes/Adapters/Adapter.php
@@ -569,7 +569,6 @@ abstract class Adapter extends \WP_Background_Process {
 		);
 
 		$import_in_progress = get_option( "cpl_{$this->type}_adapter_import_in_progress", false );
-		$field_name         = $import_in_progress ? __( 'Import in progress', 'cp-library' ) : __( 'Start full import', 'cp-library' );
 		$cmb->add_field(
 			array(
 				'name'       => __( 'Start full import', 'cp-library' ),
