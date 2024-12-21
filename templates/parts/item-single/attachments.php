483e350dccecf451636cc50ad66ee5e361b63a0a
Tanner Moushey <tanner@missionlab.dev>
2024-12-20 22:20:52 -0600

    update attachment icons

--- templates/parts/item-single/attachments.php
+++ templates/parts/item-single/attachments.php
@@ -5,15 +5,19 @@
  * @since 1.4.3
  * @package cp-library
  */
-
+use ChurchPlugins\Helpers;
 $downloads = get_post_meta( get_the_ID(), 'downloads', true );
 ?>
 
 <?php if ( ! empty( $downloads ) ) : ?>
 	<div class="cpl-single-item--attachments">
-		<?php foreach ( $downloads as $download ) : ?>
-			<a class="cpl-single-item--attachment cp-button is-light" href="<?php echo esc_url( $download['file'] ); ?>" target="_blank" rel="noopener noreferrer">
-				<span class="material-icons-outlined">attach_file</span>
+		<?php foreach ( $downloads as $download ) :
+			// is the file link on a different domain and not relative?
+			$external = str_contains( $download['file'], 'http' ) && ! str_contains( $download['file'], home_url() );
+			$icon = $external ? 'launch' : 'file';
+			?>
+			<a class="cpl-single-item--attachment cp-button is-light <?php echo $external ? 'is-external' : ''; ?>" href="<?php echo esc_url( $download['file'] ); ?>" target="_blank" rel="noopener noreferrer">
+				<?php echo Helpers::get_icon( $icon ); ?>
 				<?php echo esc_html( empty( $download['name'] ) ? basename( $download['file'] ) : $download['name'] ); ?>
 			</a>
 		<?php endforeach; ?>
